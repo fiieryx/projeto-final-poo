9f110316a042a1b89d71440cef94d50111fdbdaf
fiieryx <marciogdsf@gmail.com>
2022-06-27 14:33:35 -0300

    Primeiro Commit

---
+++ src/main/java/com/mycompany/projetofinalpoo/ProjetoFinalPOO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.projetofinalpoo;
+
+/**
+ *
+ * @author marci
+ */
+public class ProjetoFinalPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("teste");
+        System.out.println("TRO@KO");
+        System.out.println("aa");
+    }
+}
